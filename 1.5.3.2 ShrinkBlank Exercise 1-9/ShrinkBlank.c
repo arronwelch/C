f8bab8b5a0dacdac8ac3bac08216176116a4204e
arronwelch <Davego@protonmail.com>
2021-08-08 17:59:59 +0800

    Are you OK?

--- 1.5.3.2 ShrinkBlank Exercise 1-9/ShrinkBlank.c
+++ 1.5.3.2 ShrinkBlank Exercise 1-9/ShrinkBlank.c
@@ -1,25 +1,39 @@
+// #include <stdio.h>
+
+// /* 
+//  * Exercise 1-9. Write a program to copy its input to its output,
+//  * replacing each string of one or more blanks by a single blank.
+//  */
+// int main()
+// {
+//     int c;
+
+//     while ( (c = getchar()) != EOF )
+//     {
+//         if(c == ' ')//blanks
+//         {
+//             putchar(c);
+//             while ( (c = getchar()) == ' ' )
+//                 ;//a null statement
+//             putchar(c);
+//         }
+//         else
+//             putchar(c);        
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
-/* 
- * Exercise 1-9. Write a program to copy its input to its output,
- * replacing each string of one or more blanks by a single blank.
- */
 int main()
 {
-    int c;
+	int c, pre;
 
-    while ( (c = getchar()) != EOF )
-    {
-        if(c == ' ')//blanks
-        {
-            putchar(c);
-            while ( (c = getchar()) == ' ' )
-                ;//a null statement
-            putchar(c);
-        }
-        else
-            putchar(c);        
-    }
-
-    return 0;
+	while ((c = getchar()) != EOF) {
+		if (c != ' ' || pre != ' ') {
+			putchar(c);
+		}
+		pre = c;
+	}
 }
