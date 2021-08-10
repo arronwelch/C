970aee3cc71d4b9d50256000b82c8b56d4c6fee1
arronwelch <Davego@protonmail.com>
2021-08-11 00:58:01 +0800

    EOF disappear

--- 1.9.0.0 maxline/maxline.c
+++ 1.9.0.0 maxline/maxline.c
@@ -24,7 +24,7 @@ main()
 int getline(char s[], int lim)
 {
     int c, i;
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    for (i = 0; i < lim - 1 && (c = getchar()) != '~' && c != '\n'; ++i)
         s[i] = c;
     if (c == '\n')
     {
--- 1.9.0.2 Exercise 1-17/80charline.c
+++ 1.9.0.2 Exercise 1-17/80charline.c
@@ -19,7 +19,7 @@ main()
 int getline(char s[], int lim)
 {
     int c, i;
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    for (i = 0; i < lim - 1 && (c = getchar()) != '~' && c != '\n'; ++i)
         s[i] = c;
     if (c == '\n')
     {
