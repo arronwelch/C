2ae2586f904c3dd16abb83d5ec50c60be154bdb1
arronwelch <Davego@protonmail.com>
2021-08-31 04:26:17 +0800

    debugger and scientific notation or floating point %g

--- 4.3 Exercise 4-3 modulus reverse polish calculator/Ex_4_3_rpn_modulus_negative.c
+++ 4.3 Exercise 4-3 modulus reverse polish calculator/Ex_4_3_rpn_modulus_negative.c
@@ -8,6 +8,7 @@
 */
 
 #include<stdio.h>
+#include <string.h>
 #include<stdlib.h>
 #include<math.h>
 
@@ -22,51 +23,51 @@ double pop(void);
 
 int main(void)
 {
-	int type;
-	double op2;
-	char s[MAXOP];
+    int type;
+    double op2;
+    char s[MAXOP];
 
-	while((type = getop(s)) != EOF)
-	{
-		switch(type)
-		{
-				case NUMBER:
-						push(atof(s));
-						break;
-				case '+':
-						push(pop()+pop());
-						break;
-				case '*':
-						push(pop()*pop());
-						break;
-				case '-':
-						op2 = pop();
-						push(pop()-op2);
-						break;
-				case '/':
-						op2 = pop();
-						if(op2 != 0.0)
-							push(pop()/op2);
-						else
-							printf("error:zero divisor\n");
-						break;
-				case '%':
-						op2 = pop();
-						if(op2 != 0.0)
-							push(fmod(pop(),op2));
-						else
-							printf("erro:zero divisor\n");
-						break;
-				case '\n':
-						printf("\t%.8g\n",pop());
-						break;
-				default:
-						printf("error: unknown command %s\n",s);
-						break;
+    while((type = getop(s)) != EOF)
+    {
+        switch(type)
+        {
+                case NUMBER:
+                        push(atof(s));
+                        break;
+                case '+':
+                        push(pop()+pop());
+                        break;
+                case '*':
+                        push(pop()*pop());
+                        break;
+                case '-':
+                        op2 = pop();
+                        push(pop()-op2);
+                        break;
+                case '/':
+                        op2 = pop();
+                        if(op2 != 0.0)
+                            push(pop()/op2);
+                        else
+                            printf("error:zero divisor\n");
+                        break;
+                case '%':
+                        op2 = pop();
+                        if(op2 != 0.0)
+                            push(fmod(pop(),op2));
+                        else
+                            printf("erro:zero divisor\n");
+                        break;
+                case '\n':
+                        printf("\t%.8g\n",pop());//use %e or %f, whichever is shorter
+                        break;
+                default:
+                        printf("error: unknown command %s\n",s);
+                        break;
 
-		}
-	}
-	return 0;
+        }
+    }
+    return 0;
 }
 
 
@@ -77,22 +78,22 @@ double val[MAXVAL];
 
 void push(double f)
 {
-	if(sp < MAXVAL)
-		val[sp++]=f;
-	else
-		printf("error:stack full, cant push %g\n",f);
+    if(sp < MAXVAL)
+        val[sp++]=f;
+    else
+        printf("error:stack full, cant push %g\n",f);
 }
 
 
 double pop(void)
 {
-	if(sp>0)
-		return val[--sp];
-	else
-	{
-		printf("error: stack empty\n");
-		return 0.0;
-	}
+    if(sp>0)
+        return val[--sp];
+    else
+    {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
 }
 
 #include<ctype.h>
@@ -100,42 +101,47 @@ double pop(void)
 int getch(void);
 void ungetch(int);
 
-int getop(char s[]) {
-	int i, c;
-	while ((s[0] = c = getch()) == ' ' || c == '\t')
-		;
-	s[1] = '\0';
-	if (!isdigit(c) && c != '.' && c != '-')
-		return c;		// not a number
-	i = 0;
-	if (c == '-' || isdigit(c))    	// collect integer part along with '-'
-		while (isdigit(s[++i] = c = getch()))
-			;
-	if (c == '.')      	// collect fraction part
-		while (isdigit(s[++i] = c = getch()))
-			;
-	s[i] = '\0';
-	if (c != EOF)
-		ungetch(c);
-	if (strcmp(s, "-") == 0)
-		return '-';
-	return NUMBER;
+/* getop: get next character or numeric operand */
+int getop(char s[]) 
+{
+    int i, c;
+    
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';/* only wihte space */
+
+    if (!isdigit(c) && c != '.' && c != '-')
+        return c;		/* not a number */
+
+    i = 0;
+    if (c == '-' || isdigit(c))    	// collect integer part along with '-'
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')      	// collect fraction part
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    if (strcmp(s, "-") == 0)
+        return '-';
+    return NUMBER;
 }
 
 #define BUFSIZE 100
 
-char buf[BUFSIZE];
-int bufp = 0;
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0;      /* next free position in buf */
 
-int getch(void)
+int getch(void)    /* get a (possibly pushed-back) character */
 {
-	return (bufp > 0) ? buf[--bufp] : getchar();
+    return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
-void ungetch(int c)
+void ungetch(int c) /* get a (possibly pushed-back) character */
 {
-	if(bufp >= BUFSIZE)
-		printf("ungetch: too many characters\n");
-	else
-		buf[bufp++] = c;
+    if(bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
 }
