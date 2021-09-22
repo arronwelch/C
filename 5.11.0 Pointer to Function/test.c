8c05604bd6f879b10d899b370bc3c0eb7a820410
arronwelch <Davego@protonmail.com>
2021-09-22 23:51:16 +0800

    test in qsort

---
+++ 5.11.0 Pointer to Function/test.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000 /* max #lines to be sorted */
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+void qsort(void *lineptr[], int left, int right, int (*comp)(void *, void *));
+int numcmp(char *, char *);
+
+/* sort input lines */
+int main(int argc, char *argv[])
+{
+    int nlines;      /* number of input lines read */
+    int numeric = 0; /* 1 if numeric sort */
+
+    if ()
+}
