a0946c3784c6d3293959e0759dfee79d798ee174
arronwelch <Davego@protonmail.com>
2021-11-02 01:22:21 +0800

    creat close unlink

---
+++ 8.3 Open Creat Close Unlink/filecopy1.c
@@ -0,0 +1,1 @@
+filecopy1.c
--- 8.3 Open Creat Close Unlink/test.c
+++ 8.3 Open Creat Close Unlink/test.c
@@ -2,18 +2,26 @@
 #include <fcntl.h>
 // #incldeu <sys/file.h> // Berkelay(BSD) version
 
-int fd;
-// int open(char *name, int flags, int perms);
+int main()
+{
 
-char name[] = "filename";
-int flags = -1;
+    int fd;
+    int mopen(char *name, int flags, int perms);
 
+    char name[] = "filename";
+    int flags = 0;
+    int perms = 0;
 
-fd = open(name, flags, perms);
+    fd = mopen(name, flags, perms);
 
-O_RDONLY /* open for reading only */
-O_WRONLY /* open for writing only */
-O_RDWR /* open for both reading and writing */
+    flags = O_RDONLY; /* open for reading only */
+    flags = O_WRONLY; /* open for writing only */
+    flags = O_RDWR;   /* open for both reading and writing */
 
+    fd = mopen(name, O_RDONLY, 0); //open an existing file for reading
 
+    //the perms argument is always zero for the use of open that we will discuss.
 
+    int  mcreat(char *name, int perms);
+    fd = mcreat(name, perms);
+}
