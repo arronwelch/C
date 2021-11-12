3b8b38fd121b2f72907141bcbea40e432b4b4927
deft <gostnq@gmail.com>
2021-11-13 02:25:44 +0800

    correct malloc example

--- 8.7 Example - A Storage Allocator/allocator.c
+++ 8.7 Example - A Storage Allocator/allocator.c
@@ -20,3 +20,41 @@ int main()
 	printf("Hello,Ubuntu!\n");
 	return 0;
 }
+
+static Header base;			 /* empty list to get started */
+static Header *freep = NULL; /* start of free list */
+
+/* malloc: general-purpose storage allocator */
+void *malloc(unsigned nbytes)
+{
+	Header *p, *prevp;
+	Header *moreroce(unsigned);
+	unsigned nunits;
+
+	nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+	if ((prevp = freep) == NULL) /* no free list yet */
+	{
+		base.s.ptr = freep = prevp = &base; //freeptr prevptr ?
+		base.s.size = 0;
+	}
+
+	for (p = prevp->s.ptr;; prevp = p, p = p->s.ptr)
+	{
+		if (p->s.size >= nunits) /* big enough */
+		{
+			if (p->s.size == nunits) /* exactly */
+				prevp->s.ptr = p->s.ptr;
+			else
+			{ /* allocate tail end */
+				p->s.size -= nunits;
+				p += p->s.size;
+				p->s.size = nunits;
+			}
+			freep = prevp;
+			return (void *)(p + 1);
+		}
+		if (p == freep) /* wrapped around free list */
+			if ((p = moreroce(nunits)) == NULL)
+				return NULL; /* none left */
+	}
+}
