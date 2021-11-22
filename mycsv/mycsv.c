c3ce9ec2909720a5b6f113e34465ee1cb9c7520d
deft_ubuntu <gostnq@gmail.com>
2021-11-23 02:26:35 +0800

    c write to csv file

---
+++ mycsv/mycsv.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp = fopen("mydata.csv","w");
+    printf("16 Bit:\n\n");
+
+    for(int n=0; n <4;n++)
+        fprintf(fp,"%4.4d\t %.4x %.4x\n",n,n+1,n+2);
+
+    return 0;
+}
