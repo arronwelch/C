837e00c1f4cfa6764f13e2780de2b40a2a02f082
deft_ubuntu <gostnq@gmail.com>
2021-12-22 02:19:13 +0800

    tmux

--- code/Exercise in K&R/2.6 Exercise 2-2 Equivalent for loop/Ex_2_2_getline_without_and_or.c
+++ code/Exercise in K&R/2.6 Exercise 2-2 Equivalent for loop/Ex_2_2_getline_without_and_or.c
@@ -6,36 +6,35 @@
  *
  **/
 
-#include<stdio.h>
+#include <stdio.h>
 #define MAXLINE 1000
 
-int getline(char line[], int maxline);
-int mgetline(char line[],int lim);
-void copy(char to[],char from[]);
-
+int fgetline(char line[], int maxline);
+int mgetline(char line[], int lim);
+void copy(char to[], char from[]);
 
 int main(void)
 {
-    int len,max;
-    char line[MAXLINE],maxline[MAXLINE];
+    int len, max;
+    char line[MAXLINE], maxline[MAXLINE];
 
-    max =0;
+    max = 0;
 
-    while((len=mgetline(line,MAXLINE)) > 0)
+    while ((len = mgetline(line, MAXLINE)) > 0)
     {
-        if(len > max)
+        if (len > max)
         {
             max = len;
-            copy(maxline,line);
+            copy(maxline, line);
         }
     }
 
-    if(max>0)
-        printf("%s",maxline);
+    if (max > 0)
+        printf("%s", maxline);
 }
 
 /* getline: read a line into s, return length */
-int getline(char s[], int lim)
+int fgetline(char s[], int lim)
 {
     int c, i;
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
@@ -49,11 +48,12 @@ int getline(char s[], int lim)
     return i;
 }
 
-int mgetline(char s[],int lim)
+int mgetline(char s[], int lim)
 {
-    int i,c;
+    int i, c;
 
-    for(i=0; i < lim - 1 ;++i) {
+    for (i = 0; i < lim - 1; ++i)
+    {
         c = getchar();
         if (c == EOF)
             break;
@@ -62,21 +62,21 @@ int mgetline(char s[],int lim)
         s[i] = c;
     }
 
-    if(c == '\n')
+    if (c == '\n')
     {
-    s[i] = c;
-    ++i;
+        s[i] = c;
+        ++i;
     }
 
     s[i] = '\0';
     return i;
 }
 
-void copy(char to[],char from[])
+void copy(char to[], char from[])
 {
     int i;
-    i=0;
+    i = 0;
 
-    while((to[i]=from[i]) != '\0')
-    ++i;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
 }
