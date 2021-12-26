fd499d4673326853dcd740fed33260f2b942db5d
deft_ubuntu <gostnq@gmail.com>
2021-12-27 02:31:20 +0800

    c advance

--- code/Exercise in ZJUS/selectSort.c
+++ code/Exercise in ZJUS/selectSort.c
@@ -15,26 +15,57 @@ int max(int a[], int len)
     return maxid;
 }
 
+int fun(int x)
+{
+    return (x < 3 ? 1 : (fun(x - 1) + 2 * fun(x - 2)));
+}
+
 int main()
 {
     int a[] = {2, 45, 6, 12, 87, 34, 90, 24, 23, 11, 65};
     int len = sizeof(a) / sizeof(a[0]);
 
-    for( int i = len - 1; i > 0; i--)
+    for (int i = len - 1; i > 0; i--)
     {
-        int maxid = max(a, i+1);
-        //swap a[maxid],a[len-1]
+        int maxid = max(a, i + 1);
+        // swap a[maxid],a[len-1]
         int t = a[maxid];
         a[maxid] = a[i];
         a[i] = t;
     }
 
-    for(int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
 
     printf("\n");
 
+    char ch = 'A';
+    ch += 32;
+    printf("%d %c\n", 'B' + '8' - '3', 71);
+
+    printf("%d\n", fun(4) + fun(5));
+
+    int number, right_digit;
+    number = 15292;
+    if (number == 0)
+    {
+        printf("0");
+    }
+    else
+        while (number != 0)
+        {
+            right_digit = number % 10;
+            printf("%i\n", right_digit);
+            number /= 10;
+        }
+
+
+    {
+        int x = 0;
+        int y =4;
+        printf("%d,%d\n",-10<x & y<0,-17%4);
+    }
     return 0;
 }
