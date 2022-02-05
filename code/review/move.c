13016a7b7b5860a6f865e0c321bc0f7498521fe4
arronwelch <gostnq@gmail.com>
2022-02-05 23:57:47 +0800

    move

---
+++ code/review/move.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define MAX_LEN 10000
+
+int main()
+{
+    char dir;
+    int step;
+    while (scanf("%c",&dir))
+    {
+        switch (dir)
+        {
+        case 'W':
+            scanf("%d",&step);
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    scanf("%s",&str);
+    return 0;
+}
