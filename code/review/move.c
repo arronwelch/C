3978853b226ded02e32be5de436b4ee928d52160
arronwelch <gostnq@gmail.com>
2022-02-06 22:01:41 +0800

    change move

--- code/review/move.c
+++ code/review/move.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
+#include <ctype.h>
 
 #define MAX_LEN 10000
 
 int main()
 {
-    char dir;
-    int step;
-    while (scanf("%c",&dir))
+    int str[MAX_LEN],x=0,y=0;
+    while ( (str[0]=getchar()) != EOF)
     {
-        switch (dir)
+        if(str[0] == ';')
+            continue;
+        if(str[0] == 'W')// up move
         {
-        case 'W':
-            scanf("%d",&step);
-            break;
+            if(isdigit(str[1] = getchar()))
+                if(isdigit(str[2] = getchar()))
+        }
+        else if(str[0] == 'S')// down move
+            y--;
+        else if(str[0] == 'A')// left move
+        {
+            x--;
+        }
+        else if(str[0] == 'A')// right move
+        {
 
-        default:
-            break;
         }
     }
 
-    scanf("%s",&str);
     return 0;
 }
