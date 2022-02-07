498d7c39a8f803b114d5a949b5cf763bfd962248
arronwelch <gostnq@gmail.com>
2022-02-07 23:49:58 +0800

    dp

--- code/review/dynamicProgram.c
+++ code/review/dynamicProgram.c
@@ -2,6 +2,26 @@
 
 #define MAX 60
 
+/*
+dynamic programming (0-1 knapsack problem)
+
+condition:
+item number 1 2 3 4
+item volume 2 3 4 5
+item value  3 4 5 6
+------------------------------------------
+line:the last N item(0 1 2 ... N)
+column: the max volume
+------------------------------------------
+    0 1 2 3 4 5 6 7 8
+  0 0 0 0 0 0 0 0 0 0
+  1 0
+  2 0
+  3 0
+  4 0
+
+*/
+
 int main()
 {
     int N,m;
