b85d3e84b2032b5878f6f91172256eaf59ea551f
arronwelch <Davego@protonmail.com>
2022-02-14 00:23:39 +0800

    rebuilding with sliding window

--- code/review/lengthOfLongestSubstring.c
+++ code/review/lengthOfLongestSubstring.c
@@ -1,11 +1,15 @@
 
 #include <stdio.h>
+#include <string.h>
+
 #define MAX_LEN (50000+1)
 int lengthOfLongestSubstring(char * s);
 
 int main(void)
 {
+    // char s[] = "";
     char s[] = "dvdf";
+
     // char s[MAX_LEN] = {0};
     // while(scanf("%s",s))
         printf("%d",lengthOfLongestSubstring(s));
@@ -13,33 +17,67 @@ int main(void)
     return 0;
 }
 
-#define MAX_LEN (50000+1)
-int lengthOfLongestSubstring(char * s)
-{
-    int max=0,cnt = 0;
-    char substr[MAX_LEN] = {0};
-    for(int i=0; s[i+cnt] != '\0';cnt++)
+// original:
+// #define MAX_LEN (50000+1)
+// int lengthOfLongestSubstring(char * s)
+// {
+//     int max=0,cnt = 0;
+//     char substr[MAX_LEN] = {0};
+//     for(int i=0; s[i+cnt] != '\0';cnt++)
+//     {
+//         substr[cnt] = s[i+cnt];
+//         for(int j=0;j<cnt;j++)
+//         {
+//             if(substr[cnt] == substr[j])
+//             {
+//                 cnt = 0;
+//                 substr[cnt] = s[i+cnt];
+//                 i++;
+//                 cnt--;
+//                 break;
+//             }
+//         }
+//         if(cnt > max)
+//             max = cnt;
+//     }
+//     if(s[0] == '\0')
+//         return 0;
+//     else
+//         return max+1;
+// }
+
+//3. Longest Substring Without Repeating Characters
+//Given a string s, find the length of the longest substring without repeating characters.
+// rebuilding with sliding window
+int lengthOfLongestSubstring(char * s){
+    int left = 0, right = 0;// sliding window initialization
+    int max = 0;
+
+    int len = strlen(s);
+    if( len == 0 || len == 1 )
+        return len;
+
+    while(s[right+1] != '\0')
     {
-        substr[cnt] = s[i+cnt];
-        for(int j=0;j<cnt;j++)
+        right++;//right shift
+        for (int i = left; i < right; i++)
         {
-            if(substr[cnt] == substr[j])
+            if(s[i] == s[right])
             {
-                cnt = 0;
-                substr[cnt] = s[i+cnt];
-                i++;
-                cnt--;
+                left = i + 1;//right shift
                 break;
             }
         }
-        if(cnt > max)
-            max = cnt;
+        if(right-left+1 > max)
+            max = right - left + 1;
     }
-    if(s[0] == '\0')
-        return 0;
-    else
-        return max+1;
+
+    return max;
 }
+
+
+// rebuilding with sliding window & hash
+//
 
 /*
 人生的每一天都是独一无二的，爱生活，爱自己，其实就是每天简简单单的吃饭睡觉，刷刷洗洗；
