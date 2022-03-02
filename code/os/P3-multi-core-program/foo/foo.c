7aa38d6645dd93c05035e3f9866b4f914e6c1599
deft <Davego@protonmail.com>
2022-03-03 02:33:08 +0800

    inline function

--- code/os/P3-multi-core-program/foo/foo.c
+++ code/os/P3-multi-core-program/foo/foo.c
@@ -1,6 +1,13 @@
 extern int x;
+int x = 0;
 int foo() {
   int volatile t = x;
   t += 1;
   x = t;
 }
+
+int main() {
+ // int x = 0;
+  foo();
+}
+
---
+++ code/os/P3-multi-core-program/pthread/a.c
@@ -0,0 +1,8 @@
+int x;
+
+void entry(){
+  x = 0;
+}
+
+pthread_create(entry);
+pthread_create(entry);
