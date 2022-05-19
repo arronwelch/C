5856ed8cfba9b8b4a21953041e9b6827d744a0fe
deft <Davego@protonmail.com>
2022-05-19 23:50:31 +0800

    std=c90

--- code/Exercise in C Primer Plus/Chapter 2 Introducing C/Listing 2.1/first.c
+++ code/Exercise in C Primer Plus/Chapter 2 Introducing C/Listing 2.1/first.c
@@ -1,4 +1,5 @@
-// Listing 2.1 The first.c Program
+/* Listing 2.1 The first.c Program */
+// this is no in --std=c90 ISO C90
 #include <stdio.h>
 int main(void)			/* a simple program */
 {
@@ -6,6 +7,8 @@ int main(void)			/* a simple program */
 	num = 1;			/* assign a value to num */
 
 	printf("I am a simple ");	/* use the printf() function */
+	int a=0;
+	a = 2;
 	printf("computer.\n");
 	printf("My favorite number is %d because it is first.\n", num);
 
