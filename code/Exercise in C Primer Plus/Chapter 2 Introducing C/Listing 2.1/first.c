a16a228184a5036d97d312d0fe9a3a4f29328019
deft <Davego@protonmail.com>
2022-05-21 16:22:59 +0800

    new Makefile

--- code/Exercise in C Primer Plus/Chapter 2 Introducing C/Listing 2.1/first.c
+++ code/Exercise in C Primer Plus/Chapter 2 Introducing C/Listing 2.1/first.c
@@ -8,7 +8,8 @@ int main(void)			/* a simple program */
 	num = 1;			/* assign a value to num */
 
 	printf("I am a simple ");	/* use the printf() function */
-	printf("computer.\n");
+	printf("computer.\n");		/* newline character */
+	printf("computer.\b\t");	/* backspace tab */
 	printf("My favorite number is %d because it is first.\n", num);
 
 	return 0;
