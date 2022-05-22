8ff58673616f970aab3330f5e91736c472b848d3
deft <Davego@protonmail.com>
2022-05-22 23:51:20 +0800

    Have a nice sunday!

---
+++ code/Exercise in C Primer Plus/Chapter 2 Introducing C/Programming Exercises/2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("username:abcd\n");
+	printf("address:efgh\n");
+
+	return 0;
+}
---
+++ code/Exercise in C Primer Plus/Chapter 2 Introducing C/Programming Exercises/6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int toes = 10;
+	int twice_toes = toes * 2;
+	int squared_toes = toes * toes;
+	printf("toes = %d, twice_toes = %d, squared_toes = %d\n",toes, twice_toes, squared_toes);
+
+	return 0;
+}
