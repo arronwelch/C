b62573525b2dcb17065512cbdc08da32c137492e
deft <Davego@protonmail.com>
2022-06-01 22:00:57 +0800

    keywords:_Bool,_Complex,and _Imaginary

--- code/Exercise in C Primer Plus/Chapter 3 Data and C/The _Bool Type/boolType.c
+++ code/Exercise in C Primer Plus/Chapter 3 Data and C/The _Bool Type/boolType.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+/* Macro name             |  Expands to
+bool                         _Bool
+true                         Integer constant 1
+false                        Integer constant 0
+_bool_true_false_are_defined Integer constant 1
+*/
 
 int main(void)
 {
-	_Bool boolType;
-	boolType = 1 + 1 > 2;
-	printf(" 1 + 1 > 2 ? : %s\n", boolType ? "Yes" : "No");
+	bool a = true, b = false;
+	printf("a&&b = %d\n", a&&b);
+	printf("a||b = %d\n", a||b);
+	printf("!b = %d\n", !b);
 
 	return 0;
 }
