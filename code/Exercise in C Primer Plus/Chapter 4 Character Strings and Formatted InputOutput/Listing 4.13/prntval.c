45adb2f48f32e80ac39844dc921e8689981417f5
deft <Davego@protonmail.com>
2022-06-12 02:51:12 +0800

    the return value of printf()

---
+++ code/Exercise in C Primer Plus/Chapter 4 Character Strings and Formatted InputOutput/Listing 4.13/prntval.c
@@ -0,0 +1,16 @@
+// Listing 4.13 The prntval.c Program
+/* prntval.c -- finding printf()'s return value */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int bph2o = 212;
+	int rv;
+	
+	rv = printf("%d F is water's boiling point.\n", bph2o);
+	printf("The printf() function printed %d characters.\n",
+			rv);
+
+	return 0;
+}
