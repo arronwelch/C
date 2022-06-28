6daa4cd0ef59c9d04ec9e9f7548bed520de04418
deft <Davego@protonmail.com>
2022-06-28 23:55:14 +0800

    increment operator

---
+++ code/Exercise in C Primer Plus/Chapter 5 Operators, Expressions, and Statements/Listing 5.10/add_one.c
@@ -0,0 +1,19 @@
+// Listing 5.10 The add_one.c Program
+
+/* add_one.c -- incrementing: prefix and postfix */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int ultra = 0, super = 0;
+
+	while(super < 5)
+	{
+		super++;
+		++ultra;
+		printf("super = %d, ultra = %d \n", super, ultra);
+	}
+
+	return 0;
+}
