d8353ce8bef1334454752869aba29994b03a95fa
nick <Davego@protonmail.com>
2022-12-16 21:47:26 +0800

    akaedu first program

---
+++ code/Exercise in akaedu.github.io/Chapter 1. The First Program/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* main: generate some simple output */
+
+int main(void)
+{
+//	printf("Hello, world.\n");
+//	printf(1);
+	printf(0);
+	return 0;
+}
+
+/*
+$ gcc main.c
+$ ./a.out
+
+$ gcc main.c -o main
+$ ./main
+
+$ gcc -Wall main.c
+
+*/
