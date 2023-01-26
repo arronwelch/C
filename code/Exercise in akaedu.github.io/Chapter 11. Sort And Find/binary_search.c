752ffa313340393c29e9976bb5d3b538a166b86c
nick <Davego@protonmail.com>
2023-01-26 22:52:07 +0800

    binary_search test_binary_search with assert

---
+++ code/Exercise in akaedu.github.io/Chapter 11. Sort And Find/binary_search.c
@@ -0,0 +1,34 @@
+/* binary_search.c */
+
+#include <stdio.h>
+
+#define LEN 8
+int a[LEN] = { 1, 2, 2, 2, 5, 6, 8, 9 };
+
+/* Callee
+ * Contract, Precondition, Invariant, Maintenance, Postcondition
+ * Eiffel, Bertrand Meyer, Design by Contract(DbC)
+ */
+int binarysearch(int number)
+{
+	int mid, start = 0, end = LEN - 1;
+
+	while (start <= end) {
+		mid = (start + end) / 2;
+		if (a[mid] < number)
+			start = mid + 1;
+		else if (a[mid] > number)
+			end = mid - 1;
+		else
+			return mid;
+	}
+	return -1;
+}
+
+/* Caller */
+int main(void)
+{
+	printf("binary search 5: %d\n", binarysearch(5));
+	return 0;
+}
+
---
+++ code/Exercise in akaedu.github.io/Chapter 11. Sort And Find/test_binary_search.c
@@ -0,0 +1,73 @@
+/* test_binary_search.c */
+
+/* clang -DNDEBUG test_binary_search.c && ./a.out */
+//#define NDEBUG
+#include <stdio.h>
+#include <assert.h>
+
+#define LEN 8
+int a[LEN] = { 1, 7, 2, 2, 5, 6, 8, 9 };
+
+int is_sorted(void)
+{
+	int i;
+	for (i = 1; i < LEN; i++)
+		if (a[i-1] > a[i])
+			return 0;
+	return 1;
+}
+
+int mustbe(int start, int end, int number)
+{
+	int i;
+	for (i = 0; i < start; i++)
+		if (a[i] == number)
+			return 0;
+	for (i = end+1; i < LEN; i++)
+		if (a[i] == number)
+			return 0;
+	return 1;
+}
+
+int contains(int n)
+{
+	int i;
+	for (i = 0; i < LEN; i++)
+		if (a[i] == n)
+			return 1;
+	return 0;
+}
+
+/* Callee
+ * Contract, Precondition, Invariant, Maintenance, Postcondition
+ * Eiffel, Bertrand Meyer, Design by Contract(DbC)
+ */
+int binarysearch(int number)
+{
+	int mid, start = 0, end = LEN - 1;
+
+	assert(is_sorted()); /* Precondition */
+	while (start <= end) {
+		assert(mustbe(start, end, number)); /* Maintenance */
+		mid = (start + end) / 2;
+		if (a[mid] < number)
+			start = mid + 1;
+		else if (a[mid] > number)
+			end = mid - 1;
+		else {
+			assert(mid >= start && mid <= end
+				&& a[mid] == number); /* Postcondition 1 */
+			return mid;
+		}
+	}
+	assert(!contains(number)); /* Postcondition 2 */
+	return -1;
+}
+
+/* Caller */
+int main(void)
+{
+	printf("binary search 5: %d\n", binarysearch(5));
+	return 0;
+}
+
