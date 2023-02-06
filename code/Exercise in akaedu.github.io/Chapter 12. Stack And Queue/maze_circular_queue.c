6ec7e0b41a4f61bb859c960f1fd2ab77b98e796c
nick <Davego@protonmail.com>
2023-02-06 23:39:53 +0800

    circular queue

--- code/Exercise in akaedu.github.io/Chapter 12. Stack And Queue/maze_circular_queue.c
+++ code/Exercise in akaedu.github.io/Chapter 12. Stack And Queue/maze_circular_queue.c
@@ -1,7 +1,21 @@
-/* maze_queue.c
- * 1. Queue(FIFO, First In First Out):
- * 	i .Enqueue;
- * 	ii.Dequeue;
+/* maze_circular_queue.c
+ * 1. Circular Queue:
+ *
+ *	front: always points to the first element: 0
+ *	tail: always points to the next element of the last: 0
+ *
+ *	0).capacity():
+ *		MAX_LEN_QUE
+ * 	1).enqueue():
+ *		(tail + 1) % MAX_LEN_QUE
+ * 	2).dequeue():
+ *		(front + 1) % MAX_LEN_QUE
+ *	3).is_empty():
+ *		front == tail
+ *	4).is_full():
+ *		(tail + 1) % MAX_LEN_QUE == front
+ *	5).size():
+ *		(tail + MAX_LEN_QUE - front) % MAX_LEN_QUE
  * 2. BFS(Breadth First Search) with Queue
  */
 
@@ -11,22 +25,37 @@
 #define MAX_COL 5
 #define MAX_LEN_QUE (MAX_ROW * MAX_COL)
 
-struct point { int row, col, predecessor; } queue[MAX_LEN_QUE];
+struct point { int row, col; } queue[MAX_LEN_QUE];
 int head = 0, tail = 0;
-
-void enqueue(struct point p)
-{
-	queue[tail++] = p;
-}
-
-struct point dequeue(void)
-{
-	return queue[head++];
-}
 
 int is_empty(void)
 {
 	return head == tail;
+}
+
+int is_full()
+{
+	return (tail + 1) % MAX_LEN_QUE == head;
+}
+
+void enqueue(struct point p)
+{
+	if (is_full())
+		return;
+
+	queue[tail] = p;
+	tail = (tail + 1) % MAX_LEN_QUE;
+}
+
+struct point dequeue(void)
+{
+	struct point p = { -1, -1 };
+	if (!is_empty())
+	{
+		p = queue[head];
+		head = (head + 1) % MAX_LEN_QUE;
+	}
+	return p;
 }
 
 int maze[MAX_ROW][MAX_COL] = {
@@ -50,14 +79,14 @@ void print_maze(void)
 
 void visit(int row, int col)
 {
-	struct point visit_point = { row, col, head-1 };
+	struct point visit_point = { row, col };
 	maze[row][col] = 2;
 	enqueue(visit_point);
 }
 
 int main(void)
 {
-	struct point p = { 0, 0, -1 };
+	struct point p = { 0, 0};
 
 	maze[p.row][p.col] = 2;
 	enqueue(p);
@@ -83,10 +112,6 @@ int main(void)
 	}
 	if (p.row == MAX_ROW - 1 && p.col == MAX_COL - 1) {
 		printf("(%d, %d)\n", p.row, p.col);
-		while (p.predecessor != -1) {
-			p = queue[p.predecessor];
-			printf("(%d, %d)\n", p.row, p.col);
-		}
 	} else
 		printf("No path!\n");
 
