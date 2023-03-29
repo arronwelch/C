f6224ede57c3e0dfadb504fda51484af62d5e67e
banwagon_root <Davego@protonmail.com>
2023-03-29 01:36:55 -0400

    demo assembly code : hello.s

--- code/Exercise in akaedu.github.io/Chapter 19. The Relationship Between Assembly And C/README.md
+++ code/Exercise in akaedu.github.io/Chapter 19. The Relationship Between Assembly And C/README.md
@@ -29,3 +29,30 @@ gcc -m32 funcall.c -g
 gdb a.out
 ```
 ## Calling Convention(ABI, Application Binary Interface)
+
+## main Function and Startup Example Exercise
+```bash
+vim hello.s
+as hello.s -o hello.o
+ld hello.o -o hello
+
+# gcc command default outfile name
+vim main.c
+gcc -S main.c         # -> main.s
+gcc -c main.s         # -> main.o
+gcc main.o -o main    # -> main
+
+# use gcc to link the file "hello.o" which product by cmd "as hello.s -o hello.o"
+gcc hello.o -o hello.out
+
+# Startup Routine
+# /usr/lib/gcc/x86_64-redhat-linux/8/../../../../lib64/crt1.o (include _start)
+# _start need to call main function
+
+gcc main.o -o main
+ld /usr/lib/crt1.o /usr/lib/crti.o main.o -o main -lc -dynamic-linker /lib/ld-linux.so.2
+readelf /usr/lib/crt1.o
+readelf /usr/lib/crti.o
+nm /usr/lib/crt1.o
+nm /usr/lib/crti.o
+```
---
+++ code/Exercise in akaedu.github.io/Chapter 19. The Relationship Between Assembly And C/hello.s
@@ -0,0 +1,39 @@
+#PURPOSE: Simple program that exits and returns a
+#         status code back to the Linux kernel
+#
+#INPUT:   none
+#
+#OUTPUT:  returns a status code. This can be viewed
+#         by typing
+#
+#         echo $?
+#
+#         after running the program
+#
+#VARIABLES:
+#         %eax holds the system call number
+#         %ebx holds the return status
+#
+ .section .data # Assembler Directive or Pseudo-operation
+                # read/write : global variable
+ .section .text # read/executable : program code
+ .globl _start
+_start:         # Symbol : Address
+ movl $1, %eax  # this is the linux kernel command
+                # number (system call) for exiting
+                # a program
+
+ movl $4, %ebx  # this is the status number we will
+                # return to the operating system.
+                # Change this around and it will
+                # return different things to
+                # echo $?
+
+ int $0x80      # this wakes up the kernel to run
+                # the exit command
+
+# as hello.s -o hello.o # Assembler
+# ld hello.0 -o hello   # Linker
+# ./hello
+# echo $?
+# objdump -D hello.o > hello.objdump.s
