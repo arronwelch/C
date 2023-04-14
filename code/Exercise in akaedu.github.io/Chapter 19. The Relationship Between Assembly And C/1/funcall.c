602eec2a67732298e2e9ef143e2e2831b7f55b9b
banwagon_root <Davego@protonmail.com>
2023-04-14 12:00:51 -0400

    function call

---
+++ code/Exercise in akaedu.github.io/Chapter 19. The Relationship Between Assembly And C/1/funcall.c
@@ -0,0 +1,26 @@
+/*
+ * function call
+ * $ gcc main.c -g
+ * $ objdump -dS a.out | less
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int bar(int c, int d)
+{
+	int e = c + d;
+	return e;
+}
+
+int foo(int a, int b)
+{
+	return bar(a, b);
+}
+
+int main(void)
+{
+	foo(2, 3);
+//	return 0;
+	exit(-1);
+}
